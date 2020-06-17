06b51e0aa1d8cb01c5e77ffc1e7f80b6eeaae03d
Arthur Gymer <willgymerdylan@gmail.com>
2020-06-17 11:31:07 +1000

    Add the dashboard output for RWC Final Match Thread

---
+++ docs/static/js/custom-flexdashboard-components.js
@@ -0,0 +1,12 @@
+// gt tables
+window.FlexDashboardComponents.push({
+  find: function(container) {
+    return container.find('.gt-no-scroll');
+  },
+  flex: function(fillPage) {
+    return fillPage;
+  }
+});
+
+
+
